98b64f708fffda1c92dcd0821681b25aef9973ee
Bodo Kaiser <i@bodokaiser.io>
2018-11-06 14:02:33 +0100

    added 2d x map

--- interface/roessler.py
+++ interface/roessler.py
@@ -3,6 +3,8 @@ from dash.dependencies import Input, Output
 import dash_html_components as html
 import dash_core_components as core
 
+from scipy.signal import argrelmax
+
 from dynamic import roessler_naive
 from dynamic import roessler_rk4
 
@@ -20,7 +22,7 @@ layout = html.Div(children=[
             core.Dropdown(id='roessler-option-impl', options=[
                 {'label': 'Naive', 'value': 'naive'},
                 {'label': 'Runge Kutta', 'value': 'rk4'}
-            ], value='rk4')
+            ], value='naive')
         ]),
         html.Div(children=[
             html.Label(id='roessler-label-x0', children='X0'),
@@ -43,26 +45,33 @@ layout = html.Div(children=[
         core.Graph(id='roessler-graph')
     ]),
 
-    html.Div(className='sliders-4', children=[
+    html.Div(className='sliders-5', children=[
         html.Div(children=[
-            html.Label(id='roessler-label-a'),
+            html.Label(id='roessler-label-h'),
+            core.Slider(id='roessler-slider-h', included=False,
+                        min=1e-3, max=1e-1, step=1e-3, value=1e-3)
+        ]),
+        html.Div(children=[
+            html.Label(id='roessler-label-a', children='a'),
             core.Input(id='roessler-input-a', step=0.01, value=0.2,
                        type='number', inputmode='numeric')
         ]),
         html.Div(children=[
-            html.Label(id='roessler-label-b'),
+            html.Label(id='roessler-label-b', children='b'),
             core.Input(id='roessler-input-b', step=0.01, value=0.2,
                        type='number', inputmode='numeric')
         ]),
         html.Div(children=[
-            html.Label(id='roessler-label-c'),
+            html.Label(id='roessler-label-c', children='c'),
             core.Input(id='roessler-input-c', step=0.01, value=5.7,
                        type='number', inputmode='numeric')
         ]),
         html.Div(children=[
-            html.Label(id='roessler-label-h'),
-            core.Slider(id='roessler-slider-h', included=False,
-                        min=1e-5, max=1e-3, step=1e-5, value=1e-3)
+            html.Label(id='roessler-label-view', children='View'),
+            core.Dropdown(id='roessler-option-view', options=[
+                {'label': 'XY Plane', 'value': 'xy'},
+                {'label': '1D Map', 'value': 'map'}
+            ], value='xy')
         ])
     ])
 ])
@@ -76,11 +85,23 @@ def update_label_h(value):
   return f'Δt = {value}'
 
 
-def update_figure(x0, y0, z0, a, b, c, N, h, impl):
+def update_figure(x0, y0, z0, a, b, c, N, h, impl, view):
   if impl == 'naive':
     x, y, z = roessler_naive(x0, y0, z0, a, b, c, N, h)
   if impl == 'rk4':
     x, y, z = roessler_rk4(x0, y0, z0, a, b, c, N, h)
+
+  if view == 'xy':
+    u, v = x, y
+
+    utitle = 'x'
+    vtitle = 'y'
+  if view == 'map':
+    u = x[argrelmax(x, order=1)[0]][:-1]
+    v = x[argrelmax(x, order=1)[0]][1:]
+
+    utitle = 'x_{i}'
+    vtitle = 'x_{i+1}'
 
   return {
       'data': [
@@ -91,7 +112,7 @@ def update_figure(x0, y0, z0, a, b, c, N, h, impl):
               'marker': {'color': '#444', 'size': 2}
           },
           {
-              'x': x, 'y': y,
+              'x': u, 'y': v,
               'type': 'scatter',
               'mode': 'markers',
               'marker': {'color': '#444', 'size': 2}
@@ -112,12 +133,12 @@ def update_figure(x0, y0, z0, a, b, c, N, h, impl):
               }
           },
           'xaxis': {
-              'title': 'x',
+              'title': utitle,
               'anchor': 'y',
               'domain': [0.6, 1.0]
           },
           'yaxis': {
-              'title': 'y',
+              'title': vtitle,
               'anchor': 'x',
               'domain': [0.2, 0.8]
           },
@@ -145,5 +166,6 @@ def connect(app):
       Input('roessler-input-c', 'value'),
       Input('roessler-slider-n', 'value'),
       Input('roessler-slider-h', 'value'),
-      Input('roessler-option-impl', 'value')
+      Input('roessler-option-impl', 'value'),
+      Input('roessler-option-view', 'value')
   ])(update_figure)
